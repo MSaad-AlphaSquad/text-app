16a11556957e8a18d92e0bbcab293fff0534b3e2
MSaad-AlphaSquad <saad.alphasquad@gmail.com>
2021-04-20 17:24:08 +0500

    final version

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import { CSVLink} from "react-csv";
 
 const paragraph1 = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
 const paragraph2 = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
@@ -52,7 +53,7 @@ const App = () => {
 
   const handleSave = () => {
     if(text.length > 0){
-    setData([...data, { id: uuidv4(), addedText: text, selectedText : selectedText }]);
+    setData([...data, { id: uuidv4(), selectedText : selectedText , addedText: text}]);
     setText("");
     setSelectedText("")
     setInput(!input);
@@ -64,7 +65,7 @@ const App = () => {
   };
 
   return (
-    <div className="bg-blue-200 pt-10 py-10">
+    <div className="bg-blue-200 pt-10 py-10 bg-opacity-70">
       <div className="container mx-auto ">
         <div className="mx-20 my-20">
           <h1 className="text-2xl text-red-700 text-center my-8 font-bold">
@@ -150,6 +151,9 @@ const App = () => {
               </div>
             </div>
           )}
+        </div>
+        <div className="transform hover:scale-110 motion-reduce:transform-none shadow-xl bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 px-10 py-2  mx-20 text-center w-48 text-white rounded-3xl">
+        <CSVLink data={data} filename="data.csv" className="">Download file</CSVLink>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+      translate: ['motion-reduce'],
+    },
   },
   plugins: [],
 }
