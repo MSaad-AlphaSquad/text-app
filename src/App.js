d6bd1fc70129f8f420d68f24b471a2c89ea14dc1
MSaad-AlphaSquad <saad.alphasquad@gmail.com>
2021-04-26 16:01:18 +0500

    Updated v1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { CSVLink} from "react-csv";
 
-const paragraph1 = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
-const paragraph2 = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'
+const paragraph1 = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+const paragraph2 = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."
+const content = paragraph1 + paragraph2
 
 const App = () => {
   const [width, setWidth] = useState("");
@@ -14,10 +15,18 @@ const App = () => {
   const [text, setText] = useState("");
   const [data, setData] = useState([]);
   const [selectedText, setSelectedText] = useState("");
+  
+  
+
+  // const list = content.split()
 
   useEffect(() => {
+
     console.log(data);
-  }, [data]);
+
+  },[data]);
+
+  
 
   const handleMouseClick = (e) => {
     setSelectedText(window.getSelection().toString());
@@ -54,6 +63,8 @@ const App = () => {
   const handleSave = () => {
     if(text.length > 0){
     setData([...data, { id: uuidv4(), selectedText : selectedText , addedText: text}]);
+    // setStoreKey(storeKey + 1);
+    // setValue(selectedText)
     setText("");
     setSelectedText("")
     setInput(!input);
@@ -72,9 +83,7 @@ const App = () => {
             Select any Word or Phrase from the paragraph below ⬇
           </h1>
           <p className="relative" onMouseUp={(e) => handleMouseClick(e)}>
-            {paragraph1}
-            <br />
-            {paragraph2}
+           {content}
           </p>
           {button && (
             <button
